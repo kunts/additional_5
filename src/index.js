44eba6cccb4f12d1139077290dec0b3e83d6d003
Alena.Kuntsevich <ekuntsevich@yandex.ru>
2017-09-16 21:42:34 +0300

    check for different brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = [];
+  for (var i = 0; i < str.length; i++){
+    for (var j = 0; j < bracketsConfig.length; j++){
+      if (str[i] === bracketsConfig[j][0]){
+        arr.push(str[i]);
+      
+      } else if (str[i] === bracketsConfig[j][1]){
+        if (arr[arr.length - 1 ] === bracketsConfig[j][0]){
+          arr.pop();
+        }
+
+      }
+    }
+  }
+  if (arr.length === 0){
+    return true;
+  } else {
+    return false;
+  }
 }
