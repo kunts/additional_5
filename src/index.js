7ab9d43dd33356169a4346ed69b79acacb5253af
Alena.Kuntsevich <ekuntsevich@yandex.ru>
2017-09-17 09:45:33 +0300

    feature: check for equal openning and closing brackets

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
   var arr = [];
+  var flag = 0;
+
   for (var i = 0; i < str.length; i++){
     for (var j = 0; j < bracketsConfig.length; j++){
+
       if (str[i] === bracketsConfig[j][0]){
-        arr.push(str[i]);
-      
-      } else if (str[i] === bracketsConfig[j][1]){
-        if (arr[arr.length - 1 ] === bracketsConfig[j][0]){
+            if(bracketsConfig[j][0] !== bracketsConfig[j][1] ){
+               arr.push(str[i]);
+            } else if (flag === 0){
+               arr.push(str[i]);
+               flag = 1;
+            } else if(arr[arr.length -1] === bracketsConfig[j][0] && arr.length !=0){
+               flag = 0 ;
+               arr.pop();
+               break;
+          }
+
+
+      } else if (str[i] === bracketsConfig[j][1] ){
+
+        if (arr[arr.length - 1 ] === bracketsConfig[j][0] && arr.length != 0){
+
           arr.pop();
+          break;
+        } else {
+          return false;
         }
 
       }
     }
+
   }
+
   if (arr.length === 0){
     return true;
   } else {
